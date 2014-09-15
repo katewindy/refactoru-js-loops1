85f0d0b1d3448e6d943f6fda5371d96b4c7a9212
Kate Windheuser <roboticbrain@gmail.com>
2014-09-15 11:47:29 -0600

    Added answers to all 7 questions to main.js, added explanation to index.html

---
+++ main.js
@@ -0,0 +1,48 @@
+// Uncomment code for any question to run.
+
+// Question 1: Should print out 0 100 200 300 400 500
+
+// for (var i=0; i < 501; i = i+100) {
+// 	console.log(i);
+// };
+
+// Question 2: Should print 1 2 4 8 16 32 64 to the console
+
+// for (var i=1; i <=64; i=i*2) {
+// 	console.log(i);
+// };
+
+
+// Question 3: should print 1 1 1 2 2 2 3 3 3 to the console
+
+// for (var i=1; i<=3; i++) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// };
+
+// Question 4: should print 0 2 4 6 8 10
+
+// for (var i=0; i<=10; i = i + 2) {
+// 	console.log(i);
+// };
+
+// Question 5: should print 3 6 9 12 15
+
+// for (var i=3; i <=15; i+=3) {
+// 	console.log(i);
+// };
+
+// Question 6: should print 9 8 7 6 5 4 3 2 1 0
+
+// for (var i=9; i>=0; i--) {
+// 	console.log(i);
+// };
+
+// Question 7: should print 0 1 2 3 0 1 2 3 0 1 2 3
+
+for (var i=1; i <=3; i++) {
+	for (var j=0; j<=3; j++) {
+		console.log(j);
+	};
+};
